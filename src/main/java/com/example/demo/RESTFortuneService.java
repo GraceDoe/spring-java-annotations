86ff9c0b67cf99e24c11fd973b6517650d1bf3ae
Grace <grace.djago@epfedu.fr>
2018-12-27 16:13:18 +0100

    qualifier

--- src/main/java/com/example/demo/RESTFortuneService.java
+++ src/main/java/com/example/demo/RESTFortuneService.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class RESTFortuneService implements FortuneService {
     @Override
     public String getFortune() {
